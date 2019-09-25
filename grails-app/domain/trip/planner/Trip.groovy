f5bb20a6c5d6c046b460d25252b324bfbd6c3df7
jiapulidoar <jiapulidoar@gmail.com>
2019-09-24 20:46:38 -0500

    add lab

---
+++ grails-app/domain/trip/planner/Trip.groovy
@@ -0,0 +1,18 @@
+package trip.planner
+
+class Trip {
+
+	  String name;
+		String start_date; 
+		String finish_date; 
+		String airline;
+		String boarding_group;
+		String seat; 
+		String gate; 
+		String destine;
+		String origin; 
+			String ariport; 
+    static constraints = {
+
+    }
+}
---
+++ grails-app/views/trip/search.gsp
@@ -0,0 +1,6 @@
+ 
+<body>
+    <g:each var="results" in="${results}">
+        <li>${r.branchTitle}</li>
+    </g:each>
+</body>
